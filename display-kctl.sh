dbfbb8c3eb8d03b1ebe41b523660e1fc46baa44b
504BassSlapper <504bassslapper@gmail.com>
2023-11-06 10:50:26 +0100

    userfull command

--- display-kctl.sh
+++ display-kctl.sh
@@ -4,3 +4,7 @@ k config get-contexts -o name
 k get po --all-namespaces --sort-by=.metadata.creationTimestamp 
 #####Show the latest event 
 k get events --sort-by=metadata.creationTimestamp | tail -n 1
+#### write all namespaced true resources 
+ k api-resources --namespaced -o name 
+#### print the number of roles in each ns 
+k get roles --all-namespaces --no-headers | awk '{print }'| uniq -c  
