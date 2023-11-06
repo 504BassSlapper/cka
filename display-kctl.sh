69dce1fb1239f2d7a47b89ae1f7b394506ee6930
504BassSlapper <504bassslapper@gmail.com>
2023-11-06 10:15:25 +0100

    display using kubectl last events happened in cluster

--- display-kctl.sh
+++ display-kctl.sh
@@ -2,3 +2,5 @@
 k config get-contexts -o name
 #####commande to sort pods by criteria (creationtimestamp) 
 k get po --all-namespaces --sort-by=.metadata.creationTimestamp 
+#####Show the latest event 
+k get events --sort-by=metadata.creationTimestamp | tail -n 1
