9989bba0f73b86923bd551789257e455de56a840
504BassSlapper <504bassslapper@gmail.com>
2023-11-06 16:54:37 +0100

    openssl command

--- display-kctl.sh
+++ display-kctl.sh
@@ -10,3 +10,6 @@ k get events --sort-by=metadata.creationTimestamp | tail -n 1
 k get roles --all-namespaces --no-headers | awk '{print }'| uniq -c  
 #### print the number of roles in each ns 
 crictl inspect c6dad49e613f3 | jq '.info.runtimeType'
+#### openssl command 
+openssl x509 -noout --text  -in apiserver.crt 
+openssl x509 -noout --text  --in  /var/lib/kubelet/pki/kubelet-client-current.pem | grep -i  usage -A 10
