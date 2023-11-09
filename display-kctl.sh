6fad78f97e7c386b1271c08a3f7cd794c010a66a
504BassSlapper <504bassslapper@gmail.com>
2023-11-09 10:46:29 +0100

    jsonpath commands

--- display-kctl.sh
+++ display-kctl.sh
@@ -7,9 +7,25 @@ k get events --sort-by=metadata.creationTimestamp | tail -n 1
 #### write all namespaced true resources 
  k api-resources --namespaced -o name 
 #### print the number of roles in each ns 
-k get roles --all-namespaces --no-headers | awk '{print }'| uniq -c  
+k get roles --all-namespaces --no-headers | awk '{print $1}'| uniq -c  
 #### print the number of roles in each ns 
 crictl inspect c6dad49e613f3 | jq '.info.runtimeType'
 #### openssl command 
 openssl x509 -noout --text  -in apiserver.crt 
 openssl x509 -noout --text  --in  /var/lib/kubelet/pki/kubelet-client-current.pem | grep -i  usage -A 10
+
+### json path 
+kubectl config view -o jsonpath='{.users[].name}'  
+kubectl config view -o jsonpath='{.users[*].name}'   ## liste of users 
+
+
+#### helm 
+helm repo list
+helm repo update
+helm search repo nginx 
+helm search repo bitnami/nginx 
+helm show values bitnami/apache 
+helm show values bitnami/nginx
+helm show values bitnami/nginx | grep -i replica
+helm show values bitnami/apache | yq e 
+helm show values bitnami/apache | yq e
